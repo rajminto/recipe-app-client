efe5b86873fe5e9898e4cc96bc277e4f0272fa4f
matt-winzer <matt.winzer@gmail.com>
2019-04-18 13:00:14 -0600

    created recipe component

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Component Import
+import Recipe from './components/Recipe'
+
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      recipe: {}
+    }
+  }
+
+  componentDidMount() {
+    fetch('/recipeData.json')
+      .then(res => res.json())
+      .then(recipe => {
+        this.setState({ recipe })
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Recipe recipe={this.state.recipe} />
       </div>
     );
   }
---
+++ src/components/Recipe/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../../logo.svg';
+
+
+const Recipe = () => {
+  return (
+    <div>
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  )
+}
+
+export default Recipe
