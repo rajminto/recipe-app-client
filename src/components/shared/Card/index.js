5d58bde9be4c27fce79e26eaaa85ee2442271f01
matt-winzer <matt.winzer@gmail.com>
2019-05-15 11:08:24 -0600

    added styling to Card component

--- src/components/shared/Card/index.js
+++ src/components/shared/Card/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import styles from './card.module.scss'
 
 const Card = (props) => (
-  <div>
+  <div className={styles.cardContainer}>
     <h1>Hello Card</h1>
+    {props.children}
   </div>
 )
 
