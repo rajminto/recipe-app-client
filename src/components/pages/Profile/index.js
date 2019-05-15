7284bd74ca6b79a78990658cd813fbcdebf1f26a
matt-winzer <matt.winzer@gmail.com>
2019-05-15 15:46:10 -0600

    added basic styling and explainer text to Profile

--- src/components/pages/Profile/index.js
+++ src/components/pages/Profile/index.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react'
+import styles from './profile.module.scss'
 
+import Button from '../../shared/Button'
+import Card from '../../shared/Card'
+
+// TODO: store user/loggedIn information in context api
 class Profile extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loggedIn: false,
+      isLoaded: false,
+      user: {
+        id: 0,
+        name: '',
+        email: ''
+      },
+      recipes: []
+    }
+  }
+
+  componentDidMount() {
+    this.ensureAuthenticted()
+  }
+
+  ensureAuthenticted = () => {
+    fetch('http://localhost:3000/api/auth/test', {
+      method: 'GET',
+      credentials: 'include'
+    })
+      .then(res => res.json())
+      .then(response => {
+        response.user
+          ? this.setState({ isLoaded: true, loggedIn: true, user: response.user })
+          : this.setState({ isLoaded: true, loggedIn: false })
+      })
+      .catch(console.error)
+  }
+
   render() {
+    const { loggedIn, user } = this.state
+
+
     return (
-      <div>
-        <h2>Hello from Profile</h2>
-      </div>
+      <Card className={styles.profileHeaderContainer}>
+        <h1>Profile - {user.name}</h1>
+        <p>Welcome! From here you can:</p>
+        <ul>
+          <li>Create new recipes</li>
+          <li>View recipes you have created</li>
+          <li>Edit recipes you have created</li>
+          <li>Delete recipes you have created</li>
+        </ul>
+      </Card>
     )
   }
 }
