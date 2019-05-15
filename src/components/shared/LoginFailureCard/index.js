dafc885c2657e2454f5a965e72ecd843fb9f1bb9
matt-winzer <matt.winzer@gmail.com>
2019-05-15 16:27:45 -0600

    refactored LoginFailureCard back to functional component

--- src/components/shared/LoginFailureCard/index.js
+++ src/components/shared/LoginFailureCard/index.js
@@ -5,27 +5,15 @@ import { NavLink } from 'react-router-dom';
 import Card from '../Card'
 import Button from '../Button'
 
-class LoginFailureCard extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      redirectToLogin: false,
-      redirectToRegister: false
-    }
-  }
-
-  render() {
-    return (
-      <Card className={styles.loginFailureContainer}>
-        <h1>Login Failed</h1>
-        <p>Please login or register a new account.</p>
-        <div className={styles.buttonContainer}>
-          <NavLink to='/login'><Button text='Login'/></NavLink>
-          <NavLink to='/register'><Button text='Register' /></NavLink>
-        </div>
-      </Card>
-    )
-  }
-}
+const LoginFailureCard = () => (
+  <Card className={styles.loginFailureContainer}>
+    <h1>Login Failed</h1>
+    <p>Please login or register a new account.</p>
+    <div className={styles.buttonContainer}>
+      <NavLink to='/login'><Button text='Login'/></NavLink>
+      <NavLink to='/register'><Button text='Register' /></NavLink>
+    </div>
+  </Card>
+)
 
 export default LoginFailureCard
