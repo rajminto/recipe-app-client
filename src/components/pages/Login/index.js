280f8925df0abb4e384a522c43a19df5cb89a024
matt-winzer <matt.winzer@gmail.com>
2019-05-15 11:22:57 -0600

    refactored Login to use Card component

--- src/components/pages/Login/index.js
+++ src/components/pages/Login/index.js
@@ -78,33 +78,31 @@ class Login extends Component {
 
     return (
       <Card className={styles.loginFormContainer}>
-        <div>
-          <h1>Login to Account</h1>
+        <h1>Login to Account</h1>
 
-          {/* TODO: refactor to use a Message component */}
-          {message && <h3>{this.state.message}</h3>}
+        {/* TODO: refactor to use a Message component */}
+        {message && <h3>{this.state.message}</h3>}
 
-          <form onSubmit={this.handleSubmit} className={styles.loginForm}>
-            <label>Email:</label>
-            <input
-              type="email"
-              name="email"
-              onChange={this.handleChange}
-              value={this.state.email}
-              required
-            />
-            <label>Password:</label>
-            <input
-              type="password"
-              name="password"
-              onChange={this.handleChange}
-              value={this.state.password}
-              required
-              minLength="6"
-            />
-            <Button text={'Login'} />
-          </form>
-        </div>
+        <form onSubmit={this.handleSubmit} className={styles.loginForm}>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            onChange={this.handleChange}
+            value={this.state.email}
+            required
+          />
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            onChange={this.handleChange}
+            value={this.state.password}
+            required
+            minLength="6"
+          />
+          <Button text={'Login'} />
+        </form>
       </Card>
     )
   }
