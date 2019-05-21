dbd179de07ff548b0c6db18f735d3a18aa01d989
Ross Kiser <kiserr10@gmail.com>
2019-05-21 00:03:23 -0600

    fixed menu button animation and fixed animation check transition

--- src/components/shared/CheckAnimation/index.js
+++ src/components/shared/CheckAnimation/index.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
-import { TimelineMax } from 'gsap'
+import { TimelineMax, TweenMax, Power1 } from 'gsap'
 import { DrawSVGPlugin } from '../../../utils/gsap/DrawSVGPlugin'
 import styles from './check-animation.module.scss'
-import TweenLite, { Power1 } from 'gsap/TweenLite'
 import { Transition } from 'react-transition-group'
 
 
@@ -12,17 +11,18 @@ class CheckAnimation extends Component {
     this.state = {}
     this.circle = null
     this.check = null
-    this.tl = new TimelineMax()
   }
-  componentDidMount(){
-    TweenLite.set(this.circle, { drawSVG: "0%" })
-    TweenLite.set(this.check, { drawSVG: "0%" })
-    this.animateCheck()
+  
+  setCheck = () => {
+    TweenMax.set(this.circle, { drawSVG: "0%" })
+    TweenMax.set(this.check, { drawSVG: "0%", onComplete: () => {
+      this.animateCheck()
+    }})
   }
-
-
-  animateCheck() {
-    this.tl
+  
+  animateCheck = () => {
+    const tl = new TimelineMax()
+    tl
       .to(this.circle, .6, { drawSVG: "0% 100%", ease: Power1.easeIn })
       .to(this.check, .4, { drawSVG: "0% 100%", ease: Power1.easeIn })
   }
@@ -31,12 +31,23 @@ class CheckAnimation extends Component {
   render() {
     const { animationContainer, circle, check } = styles
     return (
-      <div className={animationContainer}>
-        <svg viewBox="0 0 797 797">
-          <circle ref={circle => this.circle = circle} className={circle} cx="398.5" cy="398.5" r="381.5" />
-          <path ref={check => this.check = check} className={check} d="M824.59,572.75,939.32,662c3.29,1.35,6.1,1,4.28-.48l194.64-245" transform="translate(-591.91 -155.2)" />
-        </svg>
-      </div>
+      <Transition
+        timeout={500}
+        mountOnEnter
+        unmountOnExit
+        onEntering={this.setCheck}
+        in={this.props.formValid}
+        onExit={(node) => { 
+          TweenMax.to(node, .5, { autoAlpha: 0 })
+        }}
+      >
+        <div className={animationContainer}>
+          <svg viewBox="0 0 797 797">
+            <circle ref={circle => this.circle = circle} className={circle} cx="398.5" cy="398.5" r="381.5" />
+            <path ref={check => this.check = check} className={check} d="M824.59,572.75,939.32,662c3.29,1.35,6.1,1,4.28-.48l194.64-245" transform="translate(-591.91 -155.2)" />
+          </svg>
+        </div>
+      </Transition>
     )
   }
 } 
