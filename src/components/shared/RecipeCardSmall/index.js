80e60770bb89e4bd0acaa005a65cb3b69f00dc59
matt-winzer <matt.winzer@gmail.com>
2019-05-21 01:44:21 -0600

    added recipe image to RecipeCardSmall component

--- src/components/shared/RecipeCardSmall/index.js
+++ src/components/shared/RecipeCardSmall/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import styles from './recipeCardSmall.module.scss'
 
 import Card from '../Card'
 
 const RecipeCardSmall = ({ recipe }) => {
   return (
-    <Card>
+    <Card className={styles.recipeCardSmallContainer}>
       <h3>{recipe.name}</h3>
+      <img className={styles.recipeImage} src={recipe.img_url} alt='recipe image' />
       <p>{recipe.description}</p>
       <p>{recipe.cook_time}</p>
     </Card>
