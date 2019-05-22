ecd299503981f8c932d46a519cf41f3ef424de54
matt-winzer <matt.winzer@gmail.com>
2019-05-22 00:36:50 -0600

    adjusted margin, padding, and bg color of avatar and bio cards

--- src/components/pages/Profile/ProfileHeader/index.js
+++ src/components/pages/Profile/ProfileHeader/index.js
@@ -8,14 +8,14 @@ const ProfileHeader = ({ user }) => {
     <Card className={styles.profileHeaderContainer}>
       <h1>Welcome, {user.name}!</h1>
       <div className={styles.headerContent}>
-        <Card>
+        <Card className={styles.headerCard}>
           <img src={user.avatar_url} alt=""/>
         </Card>
-        <Card>
+        <Card className={styles.headerCard}>
           <h2>About Me</h2>
           <p>{user.bio}</p>
         </Card>
-        {/* <Card>
+        {/* <Card className={styles.headerCard}>
           <p>From here you can:</p>
           <ul>
             <li>Create new recipes</li>
@@ -24,7 +24,6 @@ const ProfileHeader = ({ user }) => {
             <li>View recipes you have created and saved</li>
           </ul>
         </Card> */}
-      
       </div>
     </Card>
   )
