d4dfe01285e1851a7b9cb6bd08d01736feea79f5
matt-winzer <matt.winzer@gmail.com>
2019-07-02 18:46:33 -0600

    added src/eslintrc.js for eslint config

---
+++ src/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    'extends': 'react-app',
+    'rules': {
+        'indent': [
+            'error',
+            2
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ]
+    }
+}
