ae3139da8b50a9a26b9fd9985a5657a9095ec83f
matt-winzer <matt.winzer@gmail.com>
2020-04-08 20:27:54 -0600

    fixed typo in deployed api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 // const baseUrl = 'http://localhost:4000';
-const baseUrl = 'https://recipe-app-server.herokuapp.com/';
+const baseUrl = 'https://recipe-app-server.herokuapp.com';
 
 module.exports = {
   baseUrl
