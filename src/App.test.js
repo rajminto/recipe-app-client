646574d44b984f3c58f682af38ee00db13375649
Ross Kiser <kiserr10@gmail.com>
2020-08-30 13:27:29 -0600

    TEST SUITE PASSING BABY WITH NEW GSAP, Jest config added

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { gsap } from 'gsap';
+import { DrawSVGPlugin } from 'gsap/DrawSVGPlugin';
+import { render } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('App renders without crashing...', () => {
+  gsap.registerPlugin(DrawSVGPlugin);
+  const container = render(<App />);
+  expect(container).toMatchSnapshot();
 });
