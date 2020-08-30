4c8d9e8d74d82197a0e965d40bbfe62403baaf47
Ross Kiser <kiserr10@gmail.com>
2020-08-30 13:32:09 -0600

    fixed failing tests and config object for jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import { gsap } from 'gsap';
-import { DrawSVGPlugin } from 'gsap/DrawSVGPlugin';
 import { render } from '@testing-library/react';
 import App from './App';
 
 test('App renders without crashing...', () => {
-  gsap.registerPlugin(DrawSVGPlugin);
   const container = render(<App />);
   expect(container).toMatchSnapshot();
 });
