9b0b647bfade9750f4d2c316d4804f57994fb84d
Matt Winzer <matt.winzer@gmail.com>
2020-11-18 17:44:54 -0700

    RRAS-64 refactor Register component to use hooks

--- src/components/pages/Register/index.js
+++ src/components/pages/Register/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import styles from './register.module.scss';
 
@@ -8,119 +8,91 @@ import Card from '../../shared/Card';
 import { baseUrl } from '../../../api';
 
 // TODO?: use context api to control form
-class Register extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: '',
-      registrationSuccess: false,
-      form: {
-        name: '',
-        email: '',
-        password: '',
-        password2: ''
-      }
-    };
-  }
+const Register = () => {
+  const [message, setMessage] = useState('');
+  const [registrationSuccess, setRegistrationSuccess] = useState(false);
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
 
-  handleChange = e => {
-    this.setState({
-      form: {
-        ...this.state.form,
-        [e.target.name]: e.target.value
-      }
+  const handleChange = e => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
     });
   };
 
-  handleSubmit = e => {
-    e.preventDefault();
-    const user = this.state.form;
-
-    this.validatePasswords(user)
-      .then(this.submitNewUser)
-      .then(res => res.json())
-      .then(response => {
-        // Registration succeeded: redirect to signup page
-        // Registration failed: display response message
-        response.success
-          ? this.setState({ registrationSuccess: true })
-          : this.setState({ message: response.message });
-      })
-      .catch(err => {
-        if (typeof err === 'string') this.setState({ message: err });
-      });
-  };
-
-  submitNewUser = user => {
+  const submitNewUser = newUser => {
     return fetch(`${baseUrl}/api/auth/register`, {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
         accept: 'application/json'
       },
-      body: JSON.stringify(user)
+      body: JSON.stringify(newUser)
     });
   };
 
   // TODO?: improve validation
-  validatePasswords = user => {
+  const validatePasswords = newUser => {
     return new Promise((resolve, reject) => {
-      user.password !== user.password2 ? reject('Passwords do not match.') : resolve(user);
+      newUser.password !== newUser.password2 ? reject('Passwords do not match.') : resolve(newUser);
     });
   };
 
-  render() {
-    const { message, registrationSuccess } = this.state;
+  const handleSubmit = e => {
+    e.preventDefault();
 
-    if (registrationSuccess) return <Redirect to='/login' />;
+    validatePasswords(user)
+      .then(submitNewUser)
+      .then(res => res.json())
+      .then(response => {
+        // Registration succeeded: redirect to signup page
+        // Registration failed: display response message
+        response.success ? setRegistrationSuccess(true) : setMessage(response.message);
+      })
+      .catch(err => typeof err === 'string' && setMessage(err));
+  };
 
-    return (
-      <Card className={styles.registerFormContainer}>
-        <h1>Create an Account</h1>
+  if (registrationSuccess) return <Redirect to='/login' />;
 
-        {/* TODO: refactor to use a Message component */}
-        {message && <h3>{this.state.message}</h3>}
+  return (
+    <Card className={styles.registerFormContainer}>
+      <h1>Create an Account</h1>
 
-        <form onSubmit={this.handleSubmit} className={styles.registerForm}>
-          <label>Name:</label>
-          <input
-            type='text'
-            name='name'
-            onChange={this.handleChange}
-            value={this.state.name}
-            required
-          />
-          <label>Email:</label>
-          <input
-            type='email'
-            name='email'
-            onChange={this.handleChange}
-            value={this.state.email}
-            required
-          />
-          <label>Password:</label>
-          <input
-            type='password'
-            name='password'
-            onChange={this.handleChange}
-            value={this.state.password}
-            required
-            minLength='6'
-          />
-          <label>Repeat Password:</label>
-          <input
-            type='password'
-            name='password2'
-            onChange={this.handleChange}
-            value={this.state.password2}
-            required
-            minLength='6'
-          />
-          <Button text='Register' />
-        </form>
-      </Card>
-    );
-  }
-}
+      {/* TODO: refactor to use a Message component */}
+      {message && <h3>{message}</h3>}
+
+      <form onSubmit={handleSubmit} className={styles.registerForm}>
+        <label>Name:</label>
+        <input type='text' name='name' onChange={handleChange} value={user.name} required />
+        <label>Email:</label>
+        <input type='email' name='email' onChange={handleChange} value={user.email} required />
+        <label>Password:</label>
+        <input
+          type='password'
+          name='password'
+          onChange={handleChange}
+          value={user.password}
+          required
+          minLength='6'
+        />
+        <label>Repeat Password:</label>
+        <input
+          type='password'
+          name='password2'
+          onChange={handleChange}
+          value={user.password2}
+          required
+          minLength='6'
+        />
+        <Button text='Register' />
+      </form>
+    </Card>
+  );
+};
 
 export default Register;
